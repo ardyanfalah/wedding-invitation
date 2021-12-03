31821d52cba05d2d7d7d39be7e3c1b6659a81fc1
ardyanhfalah <ardyan.falah@sangkuriang.co.id>
2021-12-03 22:30:01 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,19 @@ module.exports = {
         poppin: ['Poppins']
       },
       backgroundImage: () => ({
-        gingko: "url('/images/gingko.png')"
+        gingko: "url('/images/gingko.png')",
+        daun1: "url('/images/daun1.png')",
+        daun2: "url('/images/daun2.png')",
+        daun3: "url('/images/daun3.png')",
+        daun4: "url('/images/daun4.png')",
+        kmbg1: "url('/images/kmbg1.png')",
+        kmbg2: "url('/images/kmbg2.png')",
+        kmbg3: "url('/images/kmbg3.png')",
+        kmbg4: "url('/images/kmbg4.png')",
+        kmbg5: "url('/images/kmbg5.png')",
+        kmbg6: "url('/images/kmbg6.png')",
+
+
       }),
       colors: {
         gold: {
