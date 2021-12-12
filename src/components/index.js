67a8965c0c0934be1147fdbc2e764d98610cd626
ardyanhfalah <ardyan.falah@sangkuriang.co.id>
2021-12-13 01:23:12 +0700

    add chat feature

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Button } from './Button'
 export { default as Link } from './Link'
 export { default as Logo } from './Logo'
 export { default as Layout } from './Layout'
+export { default as Main } from './Main'
+
